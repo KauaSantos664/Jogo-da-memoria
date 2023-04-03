a97ad464074d90ee7162d492b8b844c0929994b8
Kauã Santos664 <kauaadrianocosta74@gmail.com>
2023-04-03 15:00:50 -0300

    Adicionando propriedades do JS

---
+++ JS/login.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('.login__input');
+const button = document.querySelector('.login__button');
+
+const validateInput = ({ target }) => {
+    if (target.value.length> 1) {
+        button.removeAttribute('disabled');
+    }
+}
+input.addEventListener('input', validateInput)
